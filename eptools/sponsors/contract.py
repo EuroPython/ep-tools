ae90f64b4bb03d0846b97634e6fea8b2e34361bb
Alexandre M. Savio <alexsavio@gmail.com>
2016-02-20 18:17:41 +0100

    sponsors: update to new functions

---
+++ eptools/sponsors/contract.py
@@ -0,0 +1,34 @@
+"""
+Functions to generate sponsor agreement documents.
+"""
+
+from .data import contract_template
+from ..docstamp import create_document
+
+
+def create_sponsor_agreement(sponsor_data, template_file=None, output_dir='.'):
+    """ Call docstamp to use xelatex to produce a sponsor agreement
+    for the company in `sponsor_data`. The output will be saved
+    in output_dir.
+
+    Parameters
+    ----------
+    sponsor_data: pandas.DataFrame
+        A DataFrame with one row with the data of the sponsor.
+        Its columns must match the ones in the template_file content.
+
+    template_file: str
+        Path to the .tex template file.
+
+    output_dir: str
+        Path to the output folder.
+
+    Returns
+    -------
+    output_path
+
+    """
+    if template_file is None:
+        template_file = contract_template
+
+    return create_document(sponsor_data, template_file, output_dir)

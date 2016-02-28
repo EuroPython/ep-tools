8c3a3860692e937e78bec44ed3e1549f6810648a
Alexandre M. Savio <alexsavio@gmail.com>
2016-02-28 11:51:01 +0100

    enh: add/reorganise send_emails

---
+++ eptools/email/__init__.py
@@ -0,0 +1,2 @@
+
+from .client import EMailClient, EmailContact
---
+++ eptools/people/__init__.py
@@ -0,0 +1,6 @@
+
+from .contact import (parse_email_contact,
+                      parse_email_contact_fmt2,
+                      create_contact,
+                      read_contacts_file,
+                      )

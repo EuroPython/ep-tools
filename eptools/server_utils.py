363b62a4c98279198d5f9f90365e4b3caaff87a5
Alexandre M. Savio <alexsavio@gmail.com>
2016-02-28 11:51:14 +0100

    fix: add docker image name argument

--- eptools/server_utils.py
+++ eptools/server_utils.py
@@ -6,11 +6,25 @@ Helper functions to run commands on the epcon server.
 
 import os
 
+from .config import docker_name
 
-def epcon_exe_manage(cmd, user='root', host='epcon'):
-    os.system('ssh {}@{} docker exec arch_python_1 python manage.py {}'.format(user,
-                                                                               host,
-                                                                               cmd))
+
+def epcon_exe_manage(cmd, user='root', host='epcon', docker_name=None):
+    """ Run 'ssh `user`@`host` docker exec `doker_name` manage.py `cmd`'.
+    Parameters
+    ----------
+    cmd: str
+    user: str
+    host: str
+    docker_name: str
+    """
+    if docker_name is None:
+        docker_name = docker_name
+
+    os.system('ssh {}@{} docker exec {} python manage.py {}'.format(user,
+                                                                    host,
+                                                                    docker_name,
+                                                                    cmd))
 
 
 def epcon_fetch_file(cmd, fpath, user='root', host='epcon'):

20baf02d15442e428bfade6cf9bb418a3b60e4bd
Alexandre M. Savio <alexsavio@gmail.com>
2016-03-07 22:32:08 +0100

    people: add genderize function

--- eptools/people/fetch.py
+++ eptools/people/fetch.py
@@ -15,3 +15,20 @@ def fetch_ticketless_csv(out_filepath, conf='ep2016'):
     """ Create csv file with participants without ticket.  """
     return epcon_fetch_file(cmd='get_attendees_csv {} {}'.format(conf, 'incomplete'),
                             fpath=out_filepath)
+
+
+def genderize(first_name):
+    """ Use genderize.io to return a dictionary with the result of the probability
+    of a first name being of a man or a woman.
+    Example: {'count': 5856, 'gender': 'male', 'name': 'Alex', 'probability': '0.87'}
+
+    Parameters
+    ----------
+    first_name: str
+
+    Returns
+    -------
+    query_result: dict
+    """
+    import requests
+    return requests.get('https://api.genderize.io/', params={'name': first_name}).json()

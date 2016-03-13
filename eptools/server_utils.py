2964aed37a7fdae17dff47762d19e9c1324d7e31
Alexandre M. Savio <alexsavio@gmail.com>
2016-03-13 19:34:28 +0100

    enh: improve fetch file method

--- eptools/server_utils.py
+++ eptools/server_utils.py
@@ -4,7 +4,9 @@
 Helper functions to run commands on the epcon server.
 """
 import os
+import io
 import os.path as op
+import subprocess
 import logging as log
 
 from invoke import task
@@ -12,7 +14,8 @@ from invoke import task
 from .config import docker_name, epcon_db_path
 
 
-def epcon_exe_manage(cmd, user='root', host='europython.io', docker_name=docker_name):
+def epcon_exe_manage(cmd, user='root', host='europython.io',
+                     docker_name=docker_name):
     """ Run 'ssh `user`@`host` docker exec `doker_name` manage.py `cmd`'.
     Parameters
     ----------
@@ -20,23 +23,33 @@ def epcon_exe_manage(cmd, user='root', host='europython.io', docker_name=docker_
     user: str
     host: str
     docker_name: str
+
+    Returns
+    -------
+    stdout: str
     """
-    rmt_cmd = 'ssh {}@{} docker exec {} python manage.py {}'.format(user,
+    rmt_cmd = "ssh {}@{} docker exec {} python manage.py {}".format(user,
                                                                     host,
                                                                     docker_name,
                                                                     cmd)
     log.info('Running {}.'.format(rmt_cmd))
-    os.system(rmt_cmd)
+    proc = subprocess.Popen(rmt_cmd.split(' '), stdout=subprocess.PIPE)
+    result = [x.decode('utf8') for x in proc.stdout.readlines()]
+    return ''.join(result)
 
 
 def epcon_fetch_file(cmd, fpath, user='root', host='europython.io'):
-    """ Execute python manage.py command in epcon server to fetch a file. Will overwrite `fpath`. """
+    """ Execute python manage.py command in epcon server to fetch a file.
+    Will overwrite `fpath`. """
     if op.exists(fpath):
         os.remove(fpath)
 
-    epcon_exe_manage(cmd='{} >> {}'.format(cmd, fpath),
-                     user=user,
-                     host=host)
+    stdout = epcon_exe_manage(cmd=cmd, user=user, host=host)
+
+    log.info('Writing output to {}.'.format(fpath))
+    with io.open(fpath, 'w+') as f:
+        f.write(stdout)
+
     return fpath
 
 

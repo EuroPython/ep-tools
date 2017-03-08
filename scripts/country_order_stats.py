db409b134e0c46a948a6652b3c56c8bc8f2a84d5
Alexandre Manhaes Savio <alexsavio@gmail.com>
2017-03-08 16:59:48 +0100

    fix country_order_stats, add ticket codes

--- scripts/country_order_stats.py
+++ scripts/country_order_stats.py
@@ -6,6 +6,21 @@ pd.set_option('display.max_columns', None)
 
 TICKET_SALE_START_DATE = '2016-01-01'
 DB_FILE = '../../epcon/data/site/p3.db'
+TICKET_CODES = ('TDDC',
+                'TDDP',
+                'TDDS',
+                'TDSC',
+                'TDSP',
+                'TDSS',
+                'TESC',
+                'TESP',
+                'TESS',
+                'TRSC',
+                'TRSP',
+                'TRSS',
+                'TWDC',
+                'TWDP',
+                'TWDS',)
 
 conn = sqlite3.connect(DB_FILE)
 
@@ -14,8 +29,12 @@ c = conn.cursor()
 query = c.execute("""
 SELECT ORDER_ID, COUNTRY_ID
 FROM assopy_orderitem, assopy_order
-WHERE assopy_orderitem.order_id == assopy_order.id AND
-assopy_order.created >= date('{}')""".format(TICKET_SALE_START_DATE))
+WHERE
+assopy_orderitem.order_id == assopy_order.id AND
+assopy_orderitem.code IN {codes} AND
+assopy_order._complete == 1 AND
+assopy_order.created >= date('{date}')""".format(codes=str(TICKET_CODES),
+                                                 date=TICKET_SALE_START_DATE))
 
 countries = query.fetchall()
 
@@ -24,7 +43,10 @@ df = pd.DataFrame(countries, columns=['order_id', 'country'])
 country_is_none = pd.isnull(df['country'])
 df.loc[:, 'country'][country_is_none] = 'Others'
 
-counts = df.groupby('country').count().sort_values(by='order_id', ascending=False)
+sums = df.groupby('country').count().sort_values(by='order_id', ascending=False)
+sums = sums.reset_index()
 
-for i, row in counts.reset_index().iterrows():
+for i, row in sums.iterrows():
     print("{},{}".format(row['country'], row['order_id']))
+
+print('Total: {}.'.format(sums['order_id'].sum()))

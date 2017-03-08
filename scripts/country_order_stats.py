1095805aaee8605ba9cb3ac502522f32db0a394a
Alexandre Manhaes Savio <alexsavio@gmail.com>
2017-03-08 16:03:11 +0100

    fix country-stats script

--- scripts/country_order_stats.py
+++ scripts/country_order_stats.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python
+
 import sqlite3
 import pandas as pd
+pd.set_option('display.max_columns', None)
 
 TICKET_SALE_START_DATE = '2016-01-01'
+DB_FILE = '../../epcon/data/site/p3.db'
 
-conn = sqlite3.connect('data/site/p3.db')
+conn = sqlite3.connect(DB_FILE)
 
 c = conn.cursor()
 
 query = c.execute("""
 SELECT ORDER_ID, COUNTRY_ID
 FROM assopy_orderitem, assopy_order
-WHERE assopy_orderitem.order_id == assopy_order.id AND 
-assopy_order.created >= date(TICKET_SALE_START_DATE)"""")
+WHERE assopy_orderitem.order_id == assopy_order.id AND
+assopy_order.created >= date('{}')""".format(TICKET_SALE_START_DATE))
 
 countries = query.fetchall()
 
 df = pd.DataFrame(countries, columns=['order_id', 'country'])
+
+country_is_none = pd.isnull(df['country'])
+df.loc[:, 'country'][country_is_none] = 'Others'
 
 counts = df.groupby('country').count().sort_values(by='order_id', ascending=False)
 

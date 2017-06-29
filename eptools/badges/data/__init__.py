898d73cf6cca2c3e5441498516438c8d7de0e682
Alexandre Savio <alexandre.savio@maibornwolff.de>
2017-06-30 00:53:17 +0200

    fix the max lenght of the lines in the badges

--- eptools/badges/data/__init__.py
+++ eptools/badges/data/__init__.py
@@ -78,11 +78,11 @@ badge_color = {roles.keynote:   '40c1ea',
                roles.attendee:  'f9b00f'}
 
 # max number of chars for each line type
-maxlengths = {'name':    13,
-              'surname': 18,
-              'tagline': 26,
-              'company': 26,
-              'title':   26,
+maxlengths = {'name':    16,
+              'surname': 20,
+              'tagline': 30,
+              'company': 30,
+              'title':   30,
               }
 
 # positions in the badges

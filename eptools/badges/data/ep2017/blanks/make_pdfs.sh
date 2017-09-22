2301fe45e16cbf39a806ca21f2efaa424d2a144c
Alexandre M. Savio <alexsavio@gmail.com>
2017-09-22 20:41:04 +0200

    add ep2017 badge template files

---
+++ eptools/badges/data/ep2017/blanks/make_pdfs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+inkscape=/Applications/Inkscape.app/Contents/Resources/bin/inkscape
+inkscape=/usr/bin/inkscape
+
+for svg in *svg; do
+    pdf=${svg%.*}.pdf;
+    $inkscape --export-pdf=`pwd`/$pdf -T `pwd`/$svg;
+done
+
+# for i in *.pdf; do pdfjam --papersize "{23cm,33cm}" --noautoscale true --doublepages true -o $i $i; done
